57168b51a813f5ced5d0bf381d7e53ca423d028b
diatomsRcool <annethessen@gmail.com>
2021-07-01 12:12:25 -0600

    Delete kg-env directory

--- kg-env/bin/curie-csv.py
+++
@@ -1,4 +0,0 @@
-#!/Users/annethessen/eco-kg/kg-env/bin/python3
-# EASY-INSTALL-SCRIPT: 'prefixcommons==0.1.9','curie-csv.py'
-__requires__ = 'prefixcommons==0.1.9'
-__import__('pkg_resources').run_script('prefixcommons==0.1.9', 'curie-csv.py')
--- kg-env/bin/rst2html5.py
+++
@@ -1,4 +0,0 @@
-#!/Users/annethessen/eco-kg/kg-env/bin/python3
-# EASY-INSTALL-SCRIPT: 'docutils==0.16','rst2html5.py'
-__requires__ = 'docutils==0.16'
-__import__('pkg_resources').run_script('docutils==0.16', 'rst2html5.py')
